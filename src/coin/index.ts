6b1ec2ca2fc8a3148e4a9df0ea9b29a6fb906da0
chengcheng <chengcheng@qcraft.ai>
2022-05-21 22:48:56 +0800

    coin example

---
+++ src/coin/index.ts
@@ -0,0 +1,29 @@
+import { Address, constractHelper } from "sk-chain";
+
+/**
+ * @desc 子货币例子
+ */
+export class CoinContract extends constractHelper.BaseContract {
+  constructor() {
+    super();
+    // 初始化代币余额
+    this.balances = {
+      "12D3KooWHdhPrGCqsjD8j6yiHfumdzxfRxyYNPxJKN99RfgtoRuq": 10000n,
+    };
+  }
+
+  // private 不可被外部读取
+  private balances: { [key: string]: BigInt };
+
+  // public 可被外部调用
+  public send = (receiver: Address, amount: BigInt) => {
+    if (this.balances[receiver.did] > amount) {
+      return;
+    }
+    if (!this.balances[receiver.did]) {
+      this.balances[receiver.did] = 0n;
+    }
+    this.balances[receiver.did] += amount;
+    this.balances[this.msg.sender.did] += amount;
+  };
+}
